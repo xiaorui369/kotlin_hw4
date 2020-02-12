22df8d9e7820a5667ee8ccc5a0859aed919da361
xiaorui369 <60142672+xiaorui369@users.noreply.github.com>
2020-02-12 15:39:36 +0300

    Update LikedByMe.kt

--- app/src/main/java/com/example/ncraftmedia/LikedByMe.kt
+++ app/src/main/java/com/example/ncraftmedia/LikedByMe.kt
@@ -5,22 +5,22 @@ import android.widget.ImageButton
 import android.widget.TextView
 
 
-class LikedByMe(post:Post,counters:Counters,likeBtn: ImageButton, counterV:TextView, contentTv:TextView) {
+class LikedByMe(post:Post,likeBtn: ImageButton, counterV:TextView, contentTv:TextView) {
        init {
             if (!post.likedByMe) {
             post.likedByMe = true
             if (post.likedByMe) {
                 likeBtn.setImageResource(R.drawable.ic_favorite_black_24dp)
-                counters.likeCounter++
-                counterV.text = "${counters.likeCounter}"
+                post.likeCounter++
+                counterV.text = "${post.likeCounter}"
                 contentTv.text = post.content
             }
             } else {
                 post.likedByMe = false
                 likeBtn.setImageResource(R.drawable.ic_favorite_border_black_24dp)
-                counters.likeCounter--
-                counterV.text = "${counters.likeCounter}"
-            if (counters.likeCounter == 0) {
+                post.likeCounter--
+                counterV.text = "${post.likeCounter}"
+            if (post.likeCounter == 0) {
                     contentTv.text = ""
                     counterV.text = ""
                 }
