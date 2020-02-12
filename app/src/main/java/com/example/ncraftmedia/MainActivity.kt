5b60a0c381027fbd8eca3856fd9d2fb18be48d51
xiaorui369 <60142672+xiaorui369@users.noreply.github.com>
2020-02-12 15:27:41 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/ncraftmedia/MainActivity.kt
+++ app/src/main/java/com/example/ncraftmedia/MainActivity.kt
@@ -11,9 +11,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-    val specialPost = SpecialPost(2, "Netology", "Post from the future", "20.09.2019",false,123.0, 122.0,"Somewhere")
-    var counters = Counters(10,1,2,false, false)
-    var post = Post(1, "Netology", "First post from the future", "20.09.2019",false)
+    val specialPost = SpecialPost(2, "Netology", "Post from the future", "20.09.2019",false, 12,12, 13, false, false, 123.0,120.0,"Somewhere")
+    //var counters = Counters(10,1,2,false, false)
+    var post = Post(1, "Netology", "First post from the future", "20.09.2019",false, 10,10,10,false,false)
     fun postContent() {
         createdTv.text = post.created
         authorTv.text = post.author
@@ -33,14 +33,14 @@ class MainActivity : AppCompatActivity() {
         val counterM = findViewById<TextView>(R.id.counterMsg)
         val counterS = findViewById<TextView>(R.id.counterShare)
         val address = findViewById<TextView>(R.id.address)
-        counterM.text = counters.commentCounter.toString()
-        counterV.text = counters.likeCounter.toString()
-        counterS.text = counters.shareCounter.toString()
+        counterM.text = post.commentCounter.toString()
+        counterV.text = post.likeCounter.toString()
+        counterS.text = post.shareCounter.toString()
         address.text = specialPost.address
 
-        likeBtn.setOnClickListener{LikedByMe(post,counters,likeBtn,counterV,contentTv)}
-        messageBtn.setOnClickListener {CommentedByMe(post,counters,messageBtn,counterM,contentTv)}
-        shareBtn.setOnClickListener{SharedByMe(post,counters,shareBtn,counterS,contentTv)}
+        likeBtn.setOnClickListener{LikedByMe(post,likeBtn,counterV,contentTv)}
+        messageBtn.setOnClickListener {CommentedByMe(post,messageBtn,counterM,contentTv)}
+        shareBtn.setOnClickListener{SharedByMe(post,shareBtn,counterS,contentTv)}
 
         locationButton.setOnClickListener{
             val intent  = Intent().apply {
