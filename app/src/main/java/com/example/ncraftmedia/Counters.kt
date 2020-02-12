c10c386cc35bf043c996e16001da36647ae42bf6
xiaorui369 <60142672+xiaorui369@users.noreply.github.com>
2020-02-12 15:37:55 +0300

    Delete Counters.kt

--- app/src/main/java/com/example/ncraftmedia/Counters.kt
+++
@@ -1,7 +0,0 @@
-package com.example.ncraftmedia
-
-open class Counters(var likeCounter: Int,
-               var shareCounter: Int,
-               var commentCounter: Int,
-               var commentedByMe: Boolean,
-               var sharedByMe: Boolean)
