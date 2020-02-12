09d3c97ef4cd5c59fdeb3a4144ca012a13dce7e9
xiaorui369 <60142672+xiaorui369@users.noreply.github.com>
2020-02-12 15:39:57 +0300

    Update CommentedByMe.kt

--- app/src/main/java/com/example/ncraftmedia/CommentedByMe.kt
+++ app/src/main/java/com/example/ncraftmedia/CommentedByMe.kt
@@ -4,20 +4,20 @@ import android.widget.ImageButton
 import android.widget.TextView
 
 
-class CommentedByMe(post: Post, counters:Counters,messageBtn:ImageButton,counterM:TextView,contentTv:TextView) {
+class CommentedByMe(post: Post, messageBtn:ImageButton,counterM:TextView,contentTv:TextView) {
     init
-        {if (!counters.commentedByMe){
-            counters.commentedByMe = true
-            counters.commentCounter++
+        {if (!post.commentedByMe){
+            post.commentedByMe = true
+            post.commentCounter++
             messageBtn.setImageResource(R.drawable.ic_mode_comment_red_24dp)
-            counterM.text = "${counters.commentCounter}"
+            counterM.text = "${post.commentCounter}"
             contentTv.text = post.content
         }   else  {
-            counters.commentedByMe = false
-            counters.commentCounter--
+            post.commentedByMe = false
+            post.commentCounter--
             messageBtn.setImageResource(R.drawable.ic_mode_comment_gray_24dp)
-            counterM.text = "${counters.commentCounter}"
-            if (counters.commentCounter == 0){
+            counterM.text = "${post.commentCounter}"
+            if (post.commentCounter == 0){
                 contentTv.text = ""
                 counterM.text = ""
             }
