f3e6164cf9fb76f86182ec6c5d3833b7a7052b38
xiaorui369 <60142672+xiaorui369@users.noreply.github.com>
2020-02-12 15:38:19 +0300

    Update Post.kt

--- app/src/main/java/com/example/ncraftmedia/Post.kt
+++ app/src/main/java/com/example/ncraftmedia/Post.kt
@@ -1,3 +1,14 @@
 package com.example.ncraftmedia
 
-open class Post(open var id: Long, open var author: String, open var content: String, open var created: String, open var likedByMe: Boolean)
+open class Post(
+    open var id: Long,
+    open var author: String,
+    open var content: String,
+    open var created: String,
+    open var likedByMe: Boolean,
+    open var likeCounter: Int,
+    open var shareCounter: Int,
+    open var commentCounter: Int,
+    open var commentedByMe: Boolean,
+    open var sharedByMe: Boolean
+)
